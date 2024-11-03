507809c2234863bcabff71f072a74993a9b8a1a9
ducdh-se <ducdh.se@gmail.com>
2024-11-03 22:59:57 +0700

    update data

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const dataSelection = document.querySelector('#data-selection');
     const data1Button = document.querySelector('#data1-btn');
     const data2Button = document.querySelector('#data2-btn');
+    const data3Button = document.querySelector('#data3-btn');
 
     // Function to load JSON data and initialize quiz
     function loadDataAndStartQuiz(dataFile) {
@@ -29,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Event listeners for data selection
     data1Button.addEventListener('click', () => loadDataAndStartQuiz('./data/data1.json'));
     data2Button.addEventListener('click', () => loadDataAndStartQuiz('./data/data2.json'));
-    data2Button.addEventListener('click', () => loadDataAndStartQuiz('./data/data_ht.json'));
+    data3Button.addEventListener('click', () => loadDataAndStartQuiz('./data/data_ht.json'));
 
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
